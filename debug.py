5784e0c0d89af29e454f54809a75465e5c4cf45e
TheBloodyScreen <JayDee@TheBloodyScreen.com>
2017-07-10 03:47:25 +0200

    Added requirements/ expanded on debug
    debug now uses dbprint and has info, warning and success as parameters

--- debug.py
+++ debug.py
@@ -1,4 +1,11 @@
 from colorama import Fore
 
-def debug(string):
-    print(Fore.LIGHTRED_EX + '[DEBUG] ' + Fore.WHITE + string)
+
+def dbprint(typestring, string):
+    string = string
+    if typestring == 'info':
+        print(Fore.LIGHTRED_EX + '[DEBUG]' + Fore.LIGHTYELLOW_EX + '[INFO]' + Fore.WHITE + string)
+    elif typestring == 'warning':
+        print(Fore.LIGHTRED_EX + '[DEBUG]' + Fore.RED + '[WARNING]' + Fore.WHITE + string)
+    elif typestring == 'success':
+        print(Fore.LIGHTRED_EX + '[DEBUG]' + Fore.GREEN + '[SUCCESS]' + Fore.WHITE + string)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 import sys
 import discord
+import config
+from debug import dbprint
 from TwitterAPI import TwitterAPI
 from discord.ext import commands
 from colorama import Fore
-from config import *
-from debug import *
 import random
 
-api = TwitterAPI(appKey, appSecret, oauthToken, oauthTokenSecret)
+api = TwitterAPI(config.appKey, config.appSecret, config.oauthToken, config.oauthTokenSecret)
 description = '''A learning experience bot, written in python using discory.py .'''
 bot = commands.Bot(command_prefix='.', description=description)
 
@@ -19,13 +19,13 @@ async def on_ready():
     print(Fore.WHITE + "Name: " + Fore.GREEN + bot.user.name)
     print(Fore.WHITE + "ID: " + Fore.GREEN + bot.user.id)
     print(Fore.WHITE + '________________________')
-    debug(sys.platform)
+    dbprint('info', sys.platform)
     if sys.platform != "win32":
         await bot.change_presence(game=discord.Game(name='!info'))
-        debug('!info')
+        dbprint('info', '!info')
     else:
         await bot.change_presence(game=discord.Game(name='!info - indev'))
-        debug('!info - indev')
+        dbprint('info', '!info - indev')
     print(Fore.GREEN + "connected successfully")
     print(Fore.WHITE + '________________________')
 
@@ -36,11 +36,13 @@ async def info(ctx):
     await bot.send_typing(ctx.message.author)
     await bot.send_message(ctx.message.author, embed=discord.Embed(title='Commandlist for TheBloodyDiscord.', description="\n!info\nCauses these messages.\n\n!faq\nPosts a link to the FAQ in the channel it was used in.\n\n!twitch\nPosts a link to the twitch channel in the channel it was used in.", color=0x2AE92A))
 
+
 @bot.command(pass_context=True)
 async def faq(ctx):
     await bot.delete_message(ctx.message)
     await bot.send_typing(ctx.message.channel)
     await bot.send_message(ctx.message.channel, "The FAQ is currently being worked on. Please come back later!")
+
 
 @bot.command(pass_context=True)
 async def twitch(ctx):
@@ -48,34 +50,39 @@ async def twitch(ctx):
     await bot.send_typing(ctx.message.channel)
     await bot.send_message(ctx.message.channel, "<https://twitch.tv/thebloodyscreen>")
 
-# everything voice --start
+
+# everything voice --begin
 @bot.group(pass_context=True)
 async def voice(ctx):
     await bot.delete_message(ctx.message)
     if ctx.invoked_subcommand is None:
         await bot.say('Invalid link or subcommand')
 
+
 @voice.command(pass_context=True)
 async def play(ctx, cmd: str):
-    if cmd != None:
+    if cmd:
         global voice
         voice = await bot.join_voice_channel(ctx.message.author.voice_channel)
         global player
         player = await voice.create_ytdl_player(cmd)
         player.start()
-    
+
     else:
         await bot.say('Please specify a link!')
+
 
 @voice.command()
 async def stop():
     player.stop()
-        
+
+
 @voice.command()
 async def leave():
     player.stop()
     await voice.disconnect()
-# everything voice --stop
+# everything voice --end
+
 
 # simple twitter post command
 @bot.command(pass_context=True)
@@ -85,26 +92,31 @@ async def tweet(ctx):
         await bot.say('SUCCESS' if r.status_code == 200 else 'FAILURE')
 
     else:
-         await bot.say("You don't have permission to use this command!")
+        await bot.say("You don't have permission to use this command!")
+
 
 # simple addition
 @bot.command()
 async def add(first: int, second: int):
     await bot.say(str(first) + '+' + str(second) + ' = ' + str(first + second))
 
+
 # make simple choices
 @bot.command()
 async def choose(*choices: str):
     await bot.say(random.choice(choices))
 
+
 # spam around for a bit
 @bot.command(pass_context=True)
 async def repeat(ctx, times: int, *, content: str):
-    if 'admins' in getRoles(ctx.message.author):
+    await bot.delete_message(ctx.message)
+    if 'amins' in getRoles(ctx.message.author):
         for i in range(times):
             await bot.say(content)
     else:
-         await bot.say("You don't have permission to use this command!")   
+        await bot.say("You don't have permission to use this command!")
+
 
 # markdown helper
 @bot.event
@@ -117,6 +129,7 @@ async def on_message(message):
         await bot.send_message(message.channel, "CODE by " + str(author[0:-5]) + newMessage + "\n```")
     await bot.process_commands(message)
 
+
 def getRoles(author):
     roles = []
     for role in author.roles:
@@ -124,4 +137,4 @@ def getRoles(author):
     return roles
 
 
-bot.run(token)
+bot.run(config.token)
